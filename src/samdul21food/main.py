57c1f76dd5fa59ba4f90ef03a45324ac714308d0
hahahellooo <hahahello777@gmail.com>
2024-09-13 14:34:29 +0900

    test again

--- src/samdul21food/main.py
+++ src/samdul21food/main.py
@@ -1,14 +1,13 @@
 from typing import Union
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import datetime
+import csv
 
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
     "http://localhost:8899",
 ]
 
@@ -21,18 +20,19 @@ app.add_middleware(
 )
 
 
-
 @app.get("/")
 def read_root():
     return {'Hello':'n21'}
 
+
 @app.get("/food")
 def food(name:str):
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.datetime.now()
+    current_time = now.strftime('%Y-%m-%d %H:%M:%S')
 
     file_path=os.path.join('code','data','food.csv')
-    if not os.exists(file_path):
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=False)
         with open(file_path, mode='w', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerow['food','time']
@@ -41,7 +41,5 @@ def food(name:str):
     with open(file_path, mode='a', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow([name, current_time])
-    
+
     return {"food":name, "time": current_time}
-
-
